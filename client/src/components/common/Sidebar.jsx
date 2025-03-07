56799bd9e093e809e45e25dfe6fe098b3da3f340
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-03-07 23:47:05 +0700

    Update code 7/3/2025.

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -13,49 +13,49 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="md:flex-[2_2_0] w-18 max-w-52">
-      <div>
-        <Link to="/">
-          <XSvg className="w-12 fill-white" />
+    <div className="md:flex-[2_2_0] w-18 max-w-52 border-l border-gray-700">
+      <div className="sticky top-0 left-0 h-screen flex flex-col w-20 md:w-full">
+        <Link to="/" className="flex justify-center md:justify-start px-4 py-2">
+          <XSvg className="w-10 h-10 rounded-full fill-white hover:bg-stone-900" />
         </Link>
 
-        <ul>
-          <li>
-            <Link to="/">
-              <MdHomeFilled />
-              <span>Home</span>
+        <ul className="flex flex-col gap-3 mt-4">
+          <li className="flex justify-center md:justify-start">
+            <Link to="/" className="flex items-center px-4 py-2 gap-3 hover:bg-stone-900 transition-all duration-300 rounded-full cursor-pointer">
+              <MdHomeFilled className="w-8 h-8" />
+              <span className="text-lg hidden md:block">Home</span>
             </Link>
           </li>
             
-          <li>
-            <Link to="/notifications">
-              <IoNotifications />
-              <span>Notifications</span>
+          <li className="flex justify-center md:justify-start">
+            <Link to="/notifications" className="flex items-center px-4 py-2 gap-3 hover:bg-stone-900 transition-all duration-300 rounded-full cursor-pointer">
+              <IoNotifications className="w-8 h-8" />
+              <span className="text-lg hidden md:block">Notifications</span>
             </Link>
           </li>
 
-          <li>
-            <Link to={`/profile/${data.username}`}>
-              <FaUser />
-              <span>Profile</span>
+          <li className="flex justify-center md:justify-start">
+            <Link to={`/profile/${data.username}`} className="flex items-center px-4 py-2 gap-3 hover:bg-stone-900 transition-all duration-300 rounded-full cursor-pointer">
+              <FaUser className="w-8 h-8" />
+              <span className="text-lg hidden md:block">Profile</span>
             </Link>
           </li>
         </ul>
         {data && (
           <Link to={`/profile/${data.username}`}
-            className="">
-            <div>
-              <div className="w-8">
-                <img className="rounded-full" src={data?.profileImg || "/avatar-placeholder.png"} />
+            className="flex mx-auto items-center mt-auto mb-10 px-4 py-2 gap-3 hover:bg-stone-900 transition-all duration-300 rounded-full cursor-pointer">
+            <div className="avatar hidden md:inline-flex">
+              <div className="w-10">
+                <img className="rounded-full" src={data?.profileImg || "/avatar-placeholder.png"}  alt="profileImg"/>
               </div>
             </div>
 
-            <div>
-              <div>
-                <p>{data?.fullName}</p>
-                <p>{data?.username}</p>
+            <div className="flex justify-between items-center flex-1 gap-3">
+              <div className="hidden md:block">
+                <p className="text-white font-bold text-sm w-20 truncate">{data?.fullName}</p>
+                <p className="text-slate-500 text-sm">{data?.username}</p>
               </div>
-              <BiLogOut />
+              <BiLogOut className="w-8 h-8 cursor-pointer" />
             </div>
           </Link>
         )}
